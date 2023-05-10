72b2bdb352423df7f5ad72b9170e9d6c43537507
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-05-10 13:24:34 +0200

    Add RAM tests

---
+++ tests/ram_usage/measure_ram_usage.py
@@ -0,0 +1,25 @@
+import sys
+import time
+import psutil
+from ultralytics import YOLO
+
+def get_ram_usage():
+    return psutil.virtual_memory().used / (1024 ** 2)
+
+def export_and_measure_ram(export_format, model_path, output_file):
+    model = YOLO(model_path)
+
+    for _ in range(5):
+        model.export(format=export_format)
+        ram_usage = get_ram_usage()
+
+        with open(output_file, "a") as f:
+            f.write(f"{export_format},{ram_usage}\n")
+
+        time.sleep(1)  # Allow time for garbage collection between exports
+
+export_format = sys.argv[1]
+model_path = "yolov8n.pt"
+output_file = sys.argv[2]
+
+export_and_measure_ram(export_format, model_path, output_file)
---
+++ tests/ram_usage/run_exports_and_plot.py
@@ -0,0 +1,37 @@
+import os
+import csv
+import matplotlib.pyplot as plt
+from collections import defaultdict
+
+csv_filename = "ram_usage_data.csv"
+
+# Remove the CSV file if it exists
+if os.path.exists(csv_filename):
+    os.remove(csv_filename)
+
+export_formats = ["torchscript", "onnx", "openvino"]  #, "engine", "coreml", "saved_model", "pb", "tflite", "edgetpu", "tfjs", "paddle"]
+
+# Run measure_ram_usage.py for each export format
+for export_format in export_formats:
+    os.system(f"python measure_ram_usage.py {export_format} {csv_filename}")
+
+# Read the CSV file and store the data
+ram_usage_data = defaultdict(list)
+with open(csv_filename, "r") as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        export_format, ram_usage = row
+        ram_usage_data[export_format].append(float(ram_usage))
+
+# Plot the results
+fig, ax = plt.subplots()
+
+for export_format, ram_usages in ram_usage_data.items():
+    x_values = list(range(1, len(ram_usages) + 1))
+    ax.plot(x_values, ram_usages, label=export_format, marker='o')
+
+ax.set_xlabel("Exports")
+ax.set_ylabel("System RAM usage (MB)")
+ax.legend(loc="best")
+plt.tight_layout()
+plt.show()
